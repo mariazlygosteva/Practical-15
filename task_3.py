b6b911b84524e50e69465f4d2ae944300d008a49
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-12-10 11:30:25 +0700

    task_3.py

--- task_3.py
+++ task_3.py
@@ -9,17 +9,20 @@ def progress(a1: float, r: float, n: int) -> float:
 
     Returns:
         float: The n-th term of the arithmetic progression
-
-    Raises:
-        ValueError: If n is not a natural number (n <= 0)
     '''
     if n == 1:
         return a1
     return r + progress(a1, r, n - 1)
 
 
-a1 = float(input())
-r = float(input())
-n = int(input())
-result = progress(a1, r, n)
-print(result)
+def main() -> None:
+    '''The main function of the program.'''
+    a1 = float(input())
+    r = float(input())
+    n = int(input())
+    result = progress(a1, r, n)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
